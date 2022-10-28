14d37ca12bc5c4cc76ec6fd7dc4560c19a9a5f0e
graift-off <graf.off.it@gmail.com>
2022-10-28 13:16:38 +0300

    feat: debounce decorator was implemented with binded this

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const input = document.querySelector('.input');
+const debouncedValueOutput = document.querySelector('.debounced - value');
+const debouncedThisValueOutput = document.querySelector('.debounced - this - value');
+function debounce(f, delay) {
+  let timerId;
+  return function (...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(f.bind(this), delay, ...args);
+  }
+}
+function onChange(event) {
+  debouncedValueOutput.textContent = event.target.value;
+  debouncedThisValueOutput.textContent = this.value;
+  console.log(event.target.value);
+  console.log(this.value);
+}
+let wrapperFoo = debounce(onChange, 1000);
+input.addEventListener('input', wrapperFoo);
